2f17d62f5adf4a0719e4ab830d7b1ed6508580ee
mra <mra07@protonmail.com>
2021-04-25 23:51:25 -0500

    Arreglado error con autenticacion de firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -15,22 +15,22 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
-
-    // API de Firebase
-
-    doCreateUserWithEmailAndPassword = (email, password) =>
-      this.auth.createUserWithEmailAndPassword(email, password);
-
-    doSignInWithEmailAndPassword = (email, password) =>
-      this.auth.signInWithEmailAndPassword(email, password);
-
-    doSignOut = () => this.auth.signOut();
-
-    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-   
-    doPasswordUpdate = password =>
-      this.auth.currentUser.updatePassword(password);
   }
+
+  // API de Firebase
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+ 
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
